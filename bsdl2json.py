772de0e7d4edf3bece6faebe44f963d0fb926b61
Benedek Racz <betontalpfa@gmail.com>
2020-11-09 10:58:09 +0100

    add multi-file processing feature
     - argparse added
     - multiple input path accepted
     - output path argument

--- bsdl2json.py
+++ bsdl2json.py
@@ -20,6 +20,9 @@
 
 import json
 import sys
+import os
+import argparse
+import glob
 
 import bsdl
 
@@ -35,13 +38,65 @@ class BsdlSemantics:
         ast = parser.parse(''.join(ast), "group_table")
         return ast
 
-def main(filename):
+def main(filename, outfilename=None):
     with open(filename) as f:
         text = f.read()
         parser = bsdl.bsdlParser()
         ast = parser.parse(text, "bsdl_description", semantics=BsdlSemantics(), parseinfo=False)
-        print(json.dumps(ast.asjson()))
-
+        if outfilename is None:
+            print(json.dumps(ast.asjson()))
+        else:
+            with open(outfilename, 'w') as outfile:
+                json.dump(ast.asjson(), outfile)
 
 if __name__ == "__main__":
-    main(sys.argv[1])
+    # Based on: https://gist.github.com/89465127/5273149
+    parser = argparse.ArgumentParser(description='Parse bsdl files to json format.')
+    parser.add_argument('path', nargs='+', help='Path of a file or a folder of BSDL files.')
+    parser.add_argument('-e', '--extension', default='bsd',
+        help='File extension to filter by. (default=bsd)')
+    parser.add_argument('-o', '--out-path', default=None,
+        help='''Path of the parsed files. bsdl2json will print the parsed content to stdout if
+        out-path is not spectified.''')
+    args = parser.parse_args()
+    
+    # Parse paths
+    full_paths = [os.path.join(os.getcwd(), path) for path in args.path]
+    files = set()
+    for path in full_paths:
+        if os.path.isfile(path):
+            files.add(path)
+        else:
+            files |= set(glob.glob(path + '/*' + args.extension))
+
+    outpath = args.out_path
+
+    if outpath is None:
+        # Do not print information, it will disturb the printed, parsed data
+        verbose = False
+    else:
+        verbose = True
+
+    # Check if there is any valid input file
+    if not files:
+        print(f'ERROR: No BSDL files found at {args.path}')
+        sys.exit(-1)
+
+    # If we want to store the parsed content into a file...
+    if outpath is not None:
+        if len(files)>1:
+            # If there are multiple input files, we need the output path as directory
+            if not os.path.isdir(outpath):
+                print(f'ERROR: Please give a directory as output-path.')
+                sys.exit(-1)
+
+
+    for f in files:
+        outname = outpath
+        if outpath is not None:
+            bname = os.path.basename(f)
+            if os.path.isdir(outpath):
+                outname = os.path.join(outpath, f'{bname}.json')
+        if verbose:
+            print(f'Processing {f} -> {outname} ...')
+        main(f, outname)
